ec54baf2f2f20bc2013fcf5a7999203390eb5373
hoijui <hoijui.quaero@gmail.com>
2009-10-14 09:58:53 +0200

    actualize test run script

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-INT_HOME=/home/robin/svn_work/robin/Development/Projects/Others/spring/installs/cmake/gai/linux32/debug/AI/Interfaces/Java/0.1
-INT_JARS=${INT_HOME}/AIInterface.jar:${INT_HOME}/jlib/vecmath.jar:${INT_HOME}/jlib/jna/jna.jar
-MY_COMPILE_CP=${INT_JARS}:target/dependency/*:target/GAI-0.1-SNAPSHOT.jar
+MY_JAR=$(ls target/GAI*.jar)
+#INT_HOME=/home/robin/svn_work/robin/Development/Projects/Others/spring/installs/cmake/gai/linux32/debug/AI/Interfaces/Java/0.1
+#INT_JARS=${INT_HOME}/AIInterface.jar:${INT_HOME}/jlib/*
+#MY_COMPILE_CP=${INT_JARS}:target/dependency/*:${MY_JAR}
+MY_COMPILE_CP=$(cat target/cp.txt):${MY_JAR}
 MY_RUNTIME_CP=data/jscript:data/jconfig
-MY_CP=${MY_COMPILE_CP}:${MY_RUNTIME_CP}
+MY_CP=${MY_RUNTIME_CP}:${MY_COMPILE_CP}
+MY_MAIN_CLASS=gai.AIFactory
 
-java -cp ${MY_CP} gai.AIFactory
+java -cp ${MY_CP} ${MY_MAIN_CLASS} $@
 
